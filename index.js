6015a113105782d2e069543b71c609eb3a51edea
Oliver Umehara <oumehara@gmail.com>
2020-02-03 21:39:02 -0800

    modified index.js

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 $(function() {
+
     $('button').click(function() {
         const addedItem = $('#shopping-list-entry').val();
         $('.shopping-list').append('<li>' + addedItem + '</li>');
+
+
     })
 });
