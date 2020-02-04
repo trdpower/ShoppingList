073402c1b4a5a2b8197f90d66c1a5c3353026dcd
Oliver Umehara <oumehara@gmail.com>
2020-02-04 07:18:08 -0800

    more changes to index.js

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 $(function() {
     $('form').on('submit', function() {
+        event.preventDefault();
         let addedItem = $('#shopping-list-entry').val();
-        $('.shopping-list').append('<li>' + addedItem + '</li>');
+        $('.shopping-list').append('<li>' + addedItem + '</li>',).addClass('.shopping-item-controls');
+
+
+
+
+
     })
 });
