60d33b1247abfe0ebea6a895a263f591d5f18c32
Oliver Umehara <oumehara@gmail.com>
2020-02-04 13:51:22 -0800

    got delete item to work

--- index.js
+++ index.js
@@ -3,10 +3,14 @@ $(function() {
         event.preventDefault();
         let addedItem = $('#shopping-list-entry').val();
         $('.shopping-list').append('<li>' + addedItem + '</li>',).addClass('.shopping-item-controls');
-
-
-
-
-
-    })
+    });
+        
+    //strike-through added item
+    $('.shopping-item-toggle').on('click', function() {
+        $('.shopping-list').css("text-decoration", "strike-through");
+    });
+    //remove deleted item
+    $('.shopping-item-delete').on('click', function(){
+        $(this).closest('li').remove();
+    });
 });
