797d8b9679494d70ff9130dd46aa8beec7f44d23
Oliver Umehara <oumehara@gmail.com>
2020-02-04 20:49:00 -0800

    Buttons added to new items. Check and Delete buttons work

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
 $(function() {
-    $('form').on('submit', function(){
+    $('form').on('submit', function(event){
         event.preventDefault();
         //add item to bottom of list
         let addedItem = $('#shopping-list-entry').val();
-        $('.shopping-list').append('<li>' + addedItem + '</li>', '.shopping-item-controls').addClass('shopping-item-controls');
-        
+        $('.shopping-list').append(`<li>
+        <span class="shopping-item">${addedItem}</span>
+        <div class="shopping-item-controls">
+          <button class="shopping-item-toggle">
+            <span class="button-label">check</span>
+          </button>
+          <button class="shopping-item-delete">
+            <span class="button-label">delete</span>
+          </button>
+        </div>
+      </li>`);
     });
-        
+
     //strike-through added item
-    $('.shopping-item-toggle').on('click', function(){
-        $(this).closest('li').css("text-decoration", "line-through");
-    });
+    $('.shopping-list').on('click','.shopping-item-toggle', function(){
+        $(this).closest('li').toggleClass('shopping-item__checked');
+    })
+
     //remove deleted item
-    $('.shopping-item-delete').on('click', function(){
-        $(this).closest('li').remove();
-    });
+    $('.shopping-list').on('click','.shopping-item-delete', function(){
+        $(this).closest("li").remove();
+      });
 });
